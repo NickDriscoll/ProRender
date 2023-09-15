44faff6677b069832334912ce5165548edd932d4
Nick Driscoll <nick.a.mavros@gmail.com>
2023-09-15 15:40:12 -0400

    Split VulkanGraphicsDevice into its own files

--- ProRender/ProRender.h
+++ ProRender/ProRender.h
@@ -15,6 +15,7 @@
 #include "SDL.h"
 #include "SDL_main.h"
 #include "SDL_vulkan.h"
+#include "VulkanGraphicsDevice.h"
 
 #ifdef WIN32
 	#define VK_USE_PLATFORM_WIN32_KHR
@@ -27,29 +28,6 @@
 #define VOLK_IMPLEMENTATION
 #include "volk.h"
 
-#define FRAMES_IN_FLIGHT 2		//Number of simultaneous frames the GPU could be working on
-
-struct VulkanGraphicsDevice {
-	uint32_t graphics_queue_family_idx;
-	uint32_t compute_queue_family_idx;
-	uint32_t transfer_queue_family_idx;
-	VkInstance instance;
-	VkPhysicalDevice physical_device;
-	VkDevice device;
-	const VkAllocationCallbacks* alloc_callbacks;
-	VkCommandPool command_pool;
-	VkCommandBuffer command_buffers[FRAMES_IN_FLIGHT];
-	VkPipelineCache pipeline_cache;
-
-	//These two fields can be members of the graphics device struct because
-	//we are assuming bindless resource management
-	VkDescriptorSetLayout descriptor_set_layout;
-	VkPipelineLayout pipeline_layout;
-
-
-
-	//void thing();
-};
 
 struct Renderer {
 	
