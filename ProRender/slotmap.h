7cb2d91971f8286571c8df601af03f9fb2f19d23
Nick Driscoll <nick.a.mavros@gmail.com>
2023-12-30 22:35:17 -0500

    Fixed slotmap so that the live bit for a given index is stored at the highest bit of generation_bits[index]

--- ProRender/slotmap.h
+++ ProRender/slotmap.h
@@ -4,8 +4,9 @@
 #include <stack>
 #include <assert.h>
 
+#define LIVE_BIT 0x80000000
 #define EXTRACT_IDX(key) (key & 0xFFFFFFFF)
-#define EXTRACT_GENERATION(key) (key >> 32)
+#define EXTRACT_GENERATION(key) ((key >> 32) &= ~LIVE_BIT)
 
 #define SLOTMAP_IMPLEMENTATION  //TODO: Figure out how to remove this
 
@@ -23,7 +24,6 @@ struct slotmap {
 private:
     std::vector<T> _data = {};
     std::vector<uint32_t> generation_bits = {};
-    std::vector<uint64_t> live_bits = {};           //Nth least significant bit indicates if slot N is live TODO: should just use top gen bit for this
     std::stack<uint32_t, std::vector<uint32_t>> free_indices;
     uint32_t _count = 0;
 };
@@ -34,15 +34,11 @@ void slotmap<T>::alloc(uint32_t size) {
     assert(_data.size() == 0);
     _data.resize(size);
     generation_bits.resize(size);
-    live_bits.resize(size);
     std::vector<uint32_t> free_inds;
     free_inds.resize(size);
     //TODO: There has to be a better way!
     for (uint32_t i = 0; i < size; i++) {
         free_inds[i] = size - i - 1;
-    }
-    for (uint32_t i = 0; i < size; i++) {
-        live_bits[i] = 0;
     }
     
     free_indices = std::stack(free_inds);
@@ -61,7 +57,7 @@ T* slotmap<T>::data() {
 template<class T>
 T* slotmap<T>::get(uint64_t key) {
     uint32_t idx = EXTRACT_IDX(key);
-    uint32_t gen = static_cast<uint32_t>(EXTRACT_GENERATION(key));
+    uint32_t gen = static_cast<uint32_t>(key >> 32);
     T* d = nullptr;
     if (gen == generation_bits[idx]) {
         d = &_data[idx];
@@ -74,13 +70,8 @@ uint64_t slotmap<T>::insert(T thing) {
     uint32_t free_idx = free_indices.top();
     free_indices.pop();
     _data[free_idx] = thing;
+    generation_bits[free_idx] |= LIVE_BIT;
     uint32_t generation = generation_bits[free_idx];
-    uint32_t live_idx = free_idx / 64;
-    if (live_bits.size() <= live_idx) {
-        live_bits.push_back((uint64_t)1 << ((uint64_t)free_idx % 64));
-    } else {
-        live_bits[live_idx] |= (uint64_t)1 << ((uint64_t)free_idx % 64);
-    }
     _count += 1;
     return (static_cast<uint64_t>(generation) << 32) | static_cast<uint64_t>(free_idx);
 }
@@ -88,17 +79,15 @@ uint64_t slotmap<T>::insert(T thing) {
 template<class T>
 bool slotmap<T>::is_live(uint64_t idx) {
     if (_data.size() == 0) return false;
-    uint64_t i = idx / 64;
-    return live_bits[i] & ((uint64_t)1 << ((uint64_t)idx % 64));
+    return generation_bits[idx] & LIVE_BIT;
 }
 
 template<class T>
 void slotmap<T>::remove(uint32_t idx) {
     free_indices.push(idx);
+    generation_bits[idx] &= ~LIVE_BIT;
     generation_bits[idx] += 1;
     _count -= 1;
-    uint32_t live_idx = idx / 64;
-    live_bits[live_idx] &= ~((uint64_t)1 << ((uint64_t)idx % 64));
 }
 
 #endif
