ca8295d295d467565bb34775cbf45debe9c49946
Nick Driscoll <nick.a.mavros@gmail.com>
2024-06-18 22:01:41 -0400

    dump

--- src/utils.h
+++ src/utils.h
@@ -9,7 +9,7 @@ static constexpr float PI = 3.141592653589793f;
 #define PRORENDER_UNUSED_PARAMETER(x) (void)x
 
 #define ASSERT_OR_CRASH(pred)                                                               \
-    if ((pred) != 0) {                                                                      \
+    if ((pred) == 0) {                                                                      \
         char buffer[64];                \
         sprintf(buffer, "Fatal error occurred in %s at line %i", __FILE__, __LINE__); \
         tinyfd_messageBox("FATAL ERROR", buffer, "ok", "error", 1);           \
